5350c12364a456369b6e7cdf47464a282e5bf34e
Tony Waterfield <tonywaterfieldrva@gmail.com>
2019-12-21 16:23:35 -0500

    complete update to css, java and html files with

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TriviaGame
 Week 5 Homework JavaScript2 Assignment
+
+This trivia game uses javascript and jquery to manipulate the html page.  A timer will count down from 30 seconds to answer 5 questions.  When the submit button is clicked or the timer reaches zero the trivia questions will be checked.  A message displays the number of right and wrong answers.
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+       $("#start-button").click(function(){
+            var number = 30;
+            var rightGuesses = 0;
+            var wrongGuesses = 0;
+
+            $("#start-button").on("click", start);  
+            $("#submit").on("click", stop);  
+            $("#restart").on("click", start);  
+        
+            function start(){
+                      number = 30;
+                      counter = setInterval(timer, 1000);
+                      showMe(".question");
+                      showMe(".answers");
+                      showMe("#submit");
+                      hideMe("#start-button");
+                      hideMe(".directions");
+                      hideMe("#restart");
+                      hideMe("#results");
+                      rightGuesses = 0;
+                      wrongGuesses = 0;
+            }
+
+            function timer(){
+                    number-- ;
+                    $("#show-number").html("<h2>" + "Seconds Remaining :" + number + "</h2>" );
+                    if (number === 0){
+                      stop();
+                    
+                    }
+            }
+            
+            function stop(){
+                  clearInterval(counter); 
+                  $("#results").show();
+                  $("#restart").show();
+                  $(".question").hide();
+                  $(".answers").hide();
+                  $("#submit").hide();
+                  answerCheck();
+            }
+
+            function answerCheck() {
+              var q1val = $("input[name='q1']:checked").val();
+                if (q1val === "b") {
+                  rightGuesses++;
+                }
+                else { 
+                  wrongGuesses++;
+                }  
+                
+                var q2val = $("input[name='q2']:checked").val();
+                if (q2val === "b") {
+                  rightGuesses++;
+                }
+                else { 
+                  wrongGuesses++;
+                }  
+                var q3val = $("input[name='q3']:checked").val();
+                if (q3val === "b") {
+                  rightGuesses++;
+                }
+                else { 
+                  wrongGuesses++;
+                }  
+                var q4val = $("input[name='q4']:checked").val();
+                if (q4val === "b") {
+                  rightGuesses++;
+                }
+                else { 
+                  wrongGuesses++;
+                }  
+                var q5val = $("input[name='q5']:checked").val();
+                if (q5val === "b") {
+                  rightGuesses++;
+                }
+                else { 
+                  wrongGuesses++;
+                }  
+                console.log("Right: " , rightGuesses);
+                console.log("Wrong: " , wrongGuesses);
+                $("#results").html("<h2>" + "Results: " + rightGuesses + " Right / " + wrongGuesses + " Wrong " + "</h2>" );
+      
+              }
+
+
+
+            function hideMe(element) {
+    	          $(element).hide();
+            }
+    
+            function showMe(element) {
+              	$(element).show();
+            }
+
+  	start();
+  });
+});
+
+
+
+
